e41fdcdc2e6da4d8921a7ce6eb76107c5cb927f9
Emilian Roman <roman.emilian@outlook.com>
2019-07-04 22:16:28 +0800

    Revert "Do not cache update manifest when requesting it from a HTTP resource"
    This reverts commit ae57618b31c0f337394be9efb8fd020611a0c83a.

--- hxe/src/Update.cs
+++ hxe/src/Update.cs
@@ -22,7 +22,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
-using System.Net.Cache;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
@@ -32,7 +31,6 @@ using static System.Environment;
 using static System.IO.Compression.ZipFile;
 using static System.IO.File;
 using static System.IO.Path;
-using static System.Net.Cache.HttpRequestCacheLevel;
 using static HXE.Console;
 
 namespace HXE
@@ -78,10 +76,7 @@ namespace HXE
       {
         Info("Inferred web request manifest - " + uri);
 
-        var req = WebRequest.Create(uri);
-        req.CachePolicy = new HttpRequestCachePolicy(NoCacheNoStore);
-
-        using (var wr = (HttpWebResponse) req.GetResponse())
+        using (var wr = (HttpWebResponse) WebRequest.Create(uri).GetResponse())
         using (var rs = wr.GetResponseStream())
         using (var sr = new StreamReader(rs ?? throw new NullReferenceException("No response for manifest.")))
         {
